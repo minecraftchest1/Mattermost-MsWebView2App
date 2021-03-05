1fb23abb6af81f9ebd7aa98123516a19b39ff494
wyatt jackson <wyattcjackson2006@outlook.com>
2021-03-05 10:00:13 -0600

    Add webView. Attempt to handle newWindowRequested event.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Web.WebView2.Core;
 
 namespace Mattermost_MsWebViewApp
 {
@@ -23,6 +24,14 @@ namespace Mattermost_MsWebViewApp
         public MainWindow()
         {
             InitializeComponent();
+            //WebView2.CoreWebView2.Navigate("https://chat.reactos.org");
+            //WebView2.CoreWebView2.NewWindowRequested += WebView_NewWindowRequested;
+        }
+        private void WebView_NewWindowRequested(object sender, CoreWebView2NewWindowRequestedEventArgs e)
+        {
+            //System.Diagnostics.Process.Start(e.Uri);
+            //System.Diagnostics.Process.Start("https://bing.com");
+            MessageBox.Show(e.Uri, "NewWindowRequested", MessageBoxButton.OK, MessageBoxImage.Information);
         }
     }
 }
